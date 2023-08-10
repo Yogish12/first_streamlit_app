d3e83d6e3e882083f42b60a26a608f897bf36af6
Yogish <37934259+Yogish12@users.noreply.github.com>
2023-08-10 14:31:04 +1200

    Update streamlit_app.py
    Corrected the title and added Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 #created the main Python file
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 2 & Blueberry Oat Meal')
+streamlit.text('Kale, Spinach, and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
