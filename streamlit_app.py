7c7cbcbdd63f29bd170e5de723f3a54eb62e0f85
Yogish <37934259+Yogish12@users.noreply.github.com>
2023-08-10 15:21:12 +1200

    Update streamlit_app.py
    Display only fruits b/w Avocado and Strawberries

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Lets put a picklist here so they can choose the fruit they want to include
-sl.multiselect("Pick some fruits:", list(my_fruit_list.index))
+sl.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 # Display Table on the Page
 sl.dataframe(my_fruit_list)
