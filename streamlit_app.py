31ac4f7b5816c42d47ca192f0e6019a482ce7dfa
Yogish <37934259+Yogish12@users.noreply.github.com>
2023-08-10 15:11:43 +1200

    Update streamlit_app.py
    Added ability to pick fruits from the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,20 @@
 #created the main Python file
-import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 2 & Blueberry Oat Meal')
-streamlit.text('🥗 Kale, Spinach, and Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+import streamlit as sl
+sl.title('My Parents New Healthy Diner')
+sl.header('Breakfast Favorites')
+sl.text('🥣 Omega 2 & Blueberry Oat Meal')
+sl.text('🥗 Kale, Spinach, and Rocket Smoothie')
+sl.text('🐔 Hard-Boiled Free-Range Egg')
+sl.text('🥑🍞 Avocado Toast')
 
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
+# Lets put a picklist here so they can choose the fruit they want to include
+sl.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display Table on the Page
+sl.dataframe(my_fruit_list)
